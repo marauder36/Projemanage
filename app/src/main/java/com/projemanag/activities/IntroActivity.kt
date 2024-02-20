fed4cf86d5ccd4a0e5a2e3ad4b10810c862b9eae
Ovi <ovidiutecuceanu@gmail.com>
2024-02-20 08:34:57 +0200

    Projemanage

---
+++ app/src/main/java/com/projemanag/activities/IntroActivity.kt
@@ -0,0 +1,48 @@
+package com.projemanag.activities
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.projemanag.R
+import com.projemanag.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+   private var binding : ActivityIntroBinding? = null
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_intro)
+
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // This is used to get the file from the assets folder and set it to the title textView.
+        val typeface: Typeface =
+            Typeface.createFromAsset(assets, "carbon bl.ttf")
+        binding?.tvAppNameIntro?.typeface = typeface
+
+        binding?.btnSignInIntro?.setOnClickListener {
+
+            // Launch the sign in screen.
+            startActivity(Intent(this@IntroActivity, SignInActivity::class.java))
+        }
+
+        binding?.btnSignUpIntro?.setOnClickListener {
+
+            // Launch the sign up screen.
+            startActivity(Intent(this@IntroActivity, SignUpActivity::class.java))
+        }
+    }
+}
